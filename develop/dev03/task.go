04c478676422771abb2670ad1a613b9cac6bd56e
seethesatyrrise <seethesatyrrise@gmail.com>
2023-04-25 11:47:26 +0300

    + tasks 03, 05

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"sort"
 	"strconv"
@@ -16,14 +17,68 @@ type command struct {
 	parameters map[uint8]int
 }
 
+type text struct {
+	lines []string
+}
+
 func main() {
-	com := "sort text.txt -k -u"
-	res, err := parseCommand(com)
-	fmt.Println(res, err)
-	//name := "text.txt"
-	//text, _ := getLines(name)
-	//sortLines(text)
-	//saveInFile(text, name)
+	com := "sort ls -M"
+	err := sortFile(com)
+	fmt.Println(err)
+}
+
+func sortFile(com string) error {
+	comParsed, err := parseCommand(com)
+	if err != nil {
+		return err
+	}
+
+	t := &text{}
+	t.lines, err = getLines(comParsed.filename)
+	if err != nil {
+		return err
+	}
+
+	if _, ok := comParsed.parameters['b']; ok {
+		t.removeTailSpaces()
+	}
+
+	if _, ok := comParsed.parameters['u']; ok {
+		t.removeDuplicates()
+	}
+
+	if _, ok := comParsed.parameters['c']; ok {
+		if t.isSorted() {
+			fmt.Println("file is sorted")
+		} else {
+			fmt.Println("file is not sorted")
+		}
+	}
+
+	if _, ok := comParsed.parameters['n']; ok {
+		t.sortNumerically()
+	}
+
+	if k, ok := comParsed.parameters['k']; ok {
+		t.sortByColumn(k)
+	} else {
+		t.sortLines()
+	}
+
+	if _, ok := comParsed.parameters['M']; ok {
+		t.sortByMonth()
+	}
+
+	if _, ok := comParsed.parameters['r']; ok {
+		t.reverse()
+	}
+
+	err = t.saveInFile(comParsed.filename)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func parseCommand(com string) (*command, error) {
@@ -31,9 +86,13 @@ func parseCommand(com string) (*command, error) {
 	if w[0] != "sort" {
 		return nil, errors.New("undefined command")
 	}
+	params := make(map[uint8]int)
+	if len(w) > 2 {
+		params = getParameters(w[2:])
+	}
 	return &command{
 		filename:   w[1],
-		parameters: getParameters(w[2:]),
+		parameters: params,
 	}, nil
 }
 
@@ -58,21 +117,149 @@ func getLines(name string) ([]string, error) {
 	return text, nil
 }
 
-func sortLines(text []string) {
-	sort.Strings(text)
+func (t *text) sortLines() {
+	sort.Strings(t.lines)
 }
 
-func saveInFile(text []string, name string) {
+func (t *text) sortByColumn(k int) {
+	sort.Slice(t.lines, func(i, j int) bool {
+		iWords := strings.Fields(t.lines[i])
+		jWords := strings.Fields(t.lines[j])
+		if len(iWords) <= k {
+			return true
+		}
+		if len(jWords) <= k {
+			return false
+		}
+		fmt.Println(iWords[k], jWords[k])
+		return iWords[k] < jWords[k]
+	})
+}
+
+func (t *text) sortByMonth() {
+	months := []string{"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
+	lineMonths := make([]int, len(t.lines))
+	for i, line := range t.lines {
+		lineMonths[i] = searchMonth(line, months)
+	}
+	sort.Slice(t.lines, func(i, j int) bool {
+		iMonth := lineMonths[i]
+		if iMonth == -1 {
+			return true
+		}
+		jMonth := lineMonths[j]
+		if iMonth == -1 {
+			return false
+		}
+		return iMonth < jMonth
+	})
+}
+
+func searchMonth(s string, months []string) int {
+	for i, month := range months {
+		if strings.Contains(s, month) {
+			return i
+		}
+	}
+	return -1
+}
+
+func (t *text) sortNumerically() {
+	nums := make([]float64, len(t.lines))
+	for i, line := range t.lines {
+		nums[i] = getNumber(line)
+	}
+	sort.Slice(t.lines, func(i, j int) bool {
+		iNum := nums[i]
+		if iNum == 0 {
+			return true
+		}
+		jNum := nums[j]
+		if jNum == 0 {
+			return false
+		}
+		return iNum < jNum
+	})
+}
+
+func getNumber(line string) float64 {
+	num := 0.0
+	gotPoint := false
+	digitsAfterPoint := 1.0
+	for _, char := range line {
+		if !gotPoint && char == '.' {
+			gotPoint = true
+		}
+		if char >= '0' && char <= '9' {
+			if gotPoint {
+				num = num + float64(char-'0')/math.Pow(10, digitsAfterPoint)
+				digitsAfterPoint++
+			} else {
+				num = num*10 + float64(char-'0')
+			}
+		}
+	}
+	return num
+}
+
+func (t *text) reverse() {
+	tLen := len(t.lines) - 1
+	for i := 0; i <= tLen/2; i++ {
+		t.lines[i], t.lines[tLen-i] = t.lines[tLen-i], t.lines[i]
+	}
+}
+
+func (t *text) isSorted() bool {
+	return sort.StringsAreSorted(t.lines)
+}
+
+func (t *text) removeDuplicates() {
+	m := make(map[string]struct{})
+	for i, line := range t.lines {
+		if _, found := m[line]; found {
+			t.removeLine(i)
+		}
+		m[line] = struct{}{}
+	}
+}
+
+func (t *text) removeLine(i int) {
+	t.lines = append(t.lines[:i], t.lines[i+1:]...)
+}
+
+func (t *text) removeTailSpaces() {
+	spaces := 0
+	for i, line := range t.lines {
+		for i := len(line) - 1; i >= 0; i-- {
+			if line[i] == ' ' {
+				spaces++
+			} else {
+				break
+			}
+		}
+		t.lines[i] = t.lines[i][:len(line)-spaces]
+	}
+}
+
+func (t *text) saveInFile(name string) error {
 	file, err := os.Create("develop\\dev03\\sorted_" + name)
 	defer file.Close()
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
-	for _, line := range text {
-		file.Write([]byte(line + "\n"))
+	for i, line := range t.lines {
+		tail := ""
+		if i < len(t.lines)-1 {
+			tail = "\n"
+		}
+		_, err = file.Write([]byte(line + tail))
+		if err != nil {
+			return err
+		}
 	}
+
+	return nil
 }
 
 func getParameters(in []string) map[uint8]int {
@@ -81,11 +268,15 @@ func getParameters(in []string) map[uint8]int {
 		if p[0] != '-' {
 			continue
 		}
-		num, err := strconv.Atoi(p[2:])
-		if err != nil {
-			num = 0
+		if len(p) > 2 {
+			num, err := strconv.Atoi(p[2:])
+			if err != nil {
+				num = 0
+			}
+			params[p[1]] = num
+			continue
 		}
-		params[p[1]] = num
+		params[p[1]] = 0
 	}
 	return params
 }
---
+++ develop/dev05/task.go
@@ -0,0 +1,256 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type command struct {
+	filename   string
+	parameters map[uint8]int
+	reStr      string
+}
+
+type text struct {
+	lines      []string
+	matches    []int
+	re         *regexp.Regexp
+	ignoreCase bool `default:"false"`
+}
+
+func main() {
+	com := "grep -A1 -v -c -n '.*-r-.*' test"
+	err := grepFile(com)
+	fmt.Println(err)
+}
+
+// запуск фильтрации
+func grepFile(com string) error {
+	comParsed, err := parseCommand(com)
+	if err != nil {
+		return err
+	}
+
+	t := &text{}
+	if _, ok := comParsed.parameters['i']; ok {
+		t.re, err = regexp.Compile(strings.ToLower(comParsed.reStr))
+		if err != nil {
+			return err
+		}
+		t.ignoreCase = true
+	} else {
+		t.re, err = regexp.Compile(comParsed.reStr)
+		if err != nil {
+			return err
+		}
+	}
+
+	err = t.getLines(comParsed.filename)
+	if err != nil {
+		return err
+	}
+
+	if _, ok := comParsed.parameters['F']; ok {
+		t.matches = t.searchFixed(comParsed.reStr)
+	} else {
+		t.matches, err = t.searchLines()
+		if err != nil {
+			return err
+		}
+	}
+
+	if _, ok := comParsed.parameters['c']; ok {
+		fmt.Println("matches found:", len(t.matches))
+	}
+
+	if _, ok := comParsed.parameters['n']; ok {
+		t.printLinesIndexes()
+	}
+
+	if num, ok := comParsed.parameters['C']; ok {
+		t.addLines(num, true, true)
+	} else {
+		if num, ok := comParsed.parameters['A']; ok {
+			t.addLines(num, true, false)
+		}
+		if num, ok := comParsed.parameters['B']; ok {
+			t.addLines(num, false, true)
+		}
+	}
+
+	sort.Ints(t.matches)
+	t.removeDuplicates()
+
+	if _, ok := comParsed.parameters['v']; ok {
+		t.invertMatches()
+	}
+
+	err = t.saveInFile(comParsed.filename)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *text) addLines(num int, after bool, before bool) {
+	added := []int{}
+	for _, match := range t.matches {
+		if after {
+			for i := 1; i <= num && match+i < len(t.lines); i++ {
+				added = append(added, match+i)
+			}
+		}
+		if before {
+			for i := 1; i <= num && match+i >= 0; i++ {
+				added = append(added, match-i)
+			}
+		}
+	}
+	t.matches = append(t.matches, added...)
+}
+
+func (t *text) removeDuplicates() {
+	prev := -1
+	for i := 0; i < len(t.matches); {
+		if t.matches[i] != prev {
+			prev = t.matches[i]
+			i++
+		} else {
+			if i == len(t.matches) {
+				t.matches = t.matches[:i]
+			}
+			t.matches = append(t.matches[:i], t.matches[i+1:]...)
+		}
+	}
+}
+
+func (t *text) invertMatches() {
+	inverted := []int{}
+	for i, j := 0, 0; i < len(t.lines); i++ {
+		if j < len(t.matches) && i == t.matches[j] {
+			j++
+		} else {
+			inverted = append(inverted, i)
+		}
+	}
+	t.matches = inverted
+}
+
+func (t *text) searchLines() ([]int, error) {
+	matches := []int{}
+	for i, line := range t.lines {
+		found := t.re.MatchString(line)
+		if found {
+			matches = append(matches, i)
+		}
+	}
+	return matches, nil
+}
+
+func (t *text) searchFixed(str string) []int {
+	matches := []int{}
+	for i, line := range t.lines {
+		found := strings.Contains(line, str)
+		if found {
+			matches = append(matches, i)
+		}
+	}
+	return matches
+}
+
+func (t *text) printLinesIndexes() {
+	fmt.Print("matches found in lines: ")
+	for _, index := range t.matches {
+		fmt.Print(index, " ")
+	}
+	fmt.Println()
+}
+
+func parseCommand(com string) (*command, error) {
+	w := strings.Fields(com)
+	if w[0] != "grep" {
+		return nil, errors.New("undefined command")
+	}
+	params := make(map[uint8]int)
+	if len(w) > 3 {
+		params = getParameters(w[1 : len(w)-2])
+	}
+	re := w[len(w)-2]
+	return &command{
+		filename:   w[len(w)-1],
+		parameters: params,
+		reStr:      re[1 : len(re)-1],
+	}, nil
+}
+
+func (t *text) getLines(name string) error {
+	file, err := os.Open("develop\\dev05\\" + name)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if t.ignoreCase {
+			line = strings.ToLower(line)
+		}
+		t.lines = append(t.lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+		return err
+	}
+	return nil
+}
+
+func getParameters(in []string) map[uint8]int {
+	params := make(map[uint8]int)
+	for _, p := range in {
+		if p[0] != '-' {
+			continue
+		}
+		if len(p) > 2 {
+			num, err := strconv.Atoi(p[2:])
+			if err != nil {
+				num = 0
+			}
+			params[p[1]] = num
+			continue
+		}
+		params[p[1]] = 0
+	}
+	return params
+}
+
+func (t *text) saveInFile(name string) error {
+	file, err := os.Create("develop\\dev05\\grep_" + name)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+
+	tail := "\n"
+	for i, lineNum := range t.matches {
+		if i == len(t.matches)-1 {
+			tail = ""
+		}
+		_, err = file.Write([]byte(t.lines[lineNum] + tail))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
